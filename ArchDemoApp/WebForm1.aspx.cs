53b4401eabf047c1d8435088286e89030611ade5
yashweybee <yashparmar@weybee.in>
2023-10-27 09:14:04 +0530

    Add project files.

---
+++ ArchDemoApp/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArchDemoApp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            TextBox1.Text = "Page init";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            //TextBox1.Text = "Yash";
+            TextBox1.ForeColor = System.Drawing.Color.Red;
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Response.Write("Text changed");
+        }
+    }
+
+}
---
+++ ArchDemoApp/WebForm2.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="ArchDemoApp.WebForm2" Theme="Theme1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <a href="WebForm1.aspx">WebForm1.aspx</a><br />
+            <asp:Label ID="Label2" runat="server" Text="Hello"></asp:Label>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Hello" SkinID="MySkin"></asp:Label>
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Hello"></asp:Label>
+            <br />
+            <asp:Label ID="Label5" runat="server" Text="Hello"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
